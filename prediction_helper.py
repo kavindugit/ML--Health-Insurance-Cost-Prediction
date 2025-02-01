97d6786b1ed108f0eaa93005ce1db15a050e5566
Kavinduherarth_github <151900001+kavindugit@users.noreply.github.com>
2025-02-01 09:46:21 +0530

    update

--- prediction_helper.py
+++ prediction_helper.py
@@ -2,11 +2,11 @@ import pandas as pd
 from joblib import load
 from numpy.array_api import astype
 
-model_rest = load("artifacts\model_rest.joblib")
-model_young = load("artifacts\model_young.joblib")
+model_rest = load("artifacts/model_rest.joblib")
+model_young = load("artifacts/model_young.joblib")
 
-scaler_rest = load("artifacts\scaler_rest.joblib")
-scaler_young = load("artifacts\scaler_young.joblib")
+scaler_rest = load("artifacts/scaler_rest.joblib")
+scaler_young = load("artifacts/scaler_young.joblib")
 
 def calculate_risk_score(medical_history):
     risk_scores = {
